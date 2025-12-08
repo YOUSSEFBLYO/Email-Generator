a6a0da1d9f5d693b7476717e9ad0aae19f4f2089
youssef <ysfbl523@gmail.com>
2025-12-08 19:06:22 +0100

    initial commit

---
+++ src/main/java/com/youssef/email/EmailWhriteApplication.java
@@ -0,0 +1,15 @@
+package com.youssef.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWhriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWhriteApplication.class, args);
+
+	}
+
+
+}
